272749447e4cbf6c12aa750bbc249afad331d8b6
naokia <hexmap@gmail.com>
2018-03-10 17:15:12 +0900

    add validation

--- src/components/TodoManagement.vue
+++ src/components/TodoManagement.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>TODO List</h1>
+    <div v-if="errors.length" class="alert alert-warning">
+      <ul><li v-for="error in errors">{{ error }}</li></ul>
+    </div>
     <input v-model="newTodoText" placeholder="input your todo">
     <DateTime format="YYYY-MM-DD h:i:s" v-model='newDateTime' width="100px"></DateTime>
     <button v-on:click="add">add</button>
@@ -19,6 +22,7 @@ export default {
   components: { TodoItem, DateTime },
   data () {
     return {
+      errors: [],
       newTodoText: '',
       newDateTime: null,
       items: [
@@ -34,6 +38,10 @@ export default {
       }
     },
     add () {
+      this.errors = []
+      if (!this.newTodoText) this.errors.push('Input todo')
+      if (!this.newDateTime) this.errors.push('Select due datetime')
+      if (this.errors.length > 0) return
       this.items.push({ id: this.items.length, text: this.newTodoText, dueDateTime: this.newDateTime, done: false })
       this.newTodoText = ''
     }
